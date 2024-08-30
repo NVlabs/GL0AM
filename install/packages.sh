c19f9d70db7e9214499be208a57317b1da9c0b99
yanqingz <36521188+yanqingz@users.noreply.github.com>
2024-08-30 16:21:36 -0700

    Update packages.sh

--- install/packages.sh
+++ install/packages.sh
@@ -1,6 +1,8 @@
 #!/bin/csh
-/usr/local/bin/pip install  dgl -f https://data.dgl.ai/wheels/cu118/repo.html
-/usr/local/bin/pip install  dglgo -f https://data.dgl.ai/wheels-test/repo.html
+pip install scipy
+pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cu121
+pip install  dgl -f https://data.dgl.ai/wheels/cu118/repo.html
+pip install  dglgo -f https://data.dgl.ai/wheels-test/repo.html
 echo "os.environ['PATH'] += ':/opt/conda/lib'" >> /usr/local/etc/jupyter/jupyter_notebook_config.py
 echo "os.environ['LD_LIBRARY_PATH'] += ':/opt/conda/lib'" >> /usr/local/etc/jupyter/jupyter_notebook_config.py
 cat /usr/local/etc/jupyter/jupyter_notebook_config.py
