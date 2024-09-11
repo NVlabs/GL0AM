7251eb4c544ff64d717d0db4876365ed014db7c5
yanqingz <36521188+yanqingz@users.noreply.github.com>
2024-09-11 16:58:49 -0700

    Update packages.sh

--- install/packages.sh
+++ install/packages.sh
@@ -1,4 +1,13 @@
 #!/bin/csh
+##############################################################################
+# Copyright (c) 2024, NVIDIA Corporation. All rights reserved.
+#
+# This work is made available under the Nvidia Source Code License-NC.
+# To view a copy of this license, visit
+# https://github.com/NVlabs/GL0AM/blob/main/LICENSE
+#
+##############################################################################
+
 pip install scipy
 pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cu121
 pip install  dgl -f https://data.dgl.ai/wheels/cu118/repo.html
